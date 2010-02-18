d4d5cad8e1aae2600fd8cfdb6861dabdb8457d2e
Karrick McDermott <karrick@karrick.net>
2010-02-18 03:02:39 -0500

    standardized the test_helpers.rb file

--- test/test_helpers.rb
+++ test/test_helpers.rb
@@ -1,6 +1,33 @@
-require 'test/unit'
-require 'rubygems'
-require 'mocha'
-require 'flexmock/test_unit'
+# -*- mode: ruby; compile-command: "rake test"; -*-
 
-$LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib')))
+
+class Test::Unit::TestCase
+
+  FIXTURES_DIR = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
+
+  ################
+  # HELPER FUNCTIONS
+  ################
+
+  def array_hash_test (array_of_hashes)
+    # given Array of Hashes, each possessing :in and :exp key/value pairs,
+    # yield value of :in to called, and assert that result matches :exp value
+    array_of_hashes.each do |e|
+      assert_equal(e[:exp], yield(e[:in]), "CASE: #{e[:in].inspect}")
+    end
+  end
+
+  def using (*filenames)
+    raise "filenames = #{filenames.inspect}" unless filenames.kind_of?(Array)
+    filenames.flatten!
+
+    pwd = Dir.pwd
+    filenames.each do |x|
+      fixture = File.join(FIXTURES_DIR, File.basename(x))
+      FileUtils.cp(fixture, pwd)
+    end
+  end
+end
